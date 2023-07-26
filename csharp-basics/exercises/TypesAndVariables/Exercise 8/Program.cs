3c3b48765283d3f75d9476243b40236af6f75c5c
Elīza Ivenševa <e.ivenseva@gmail.com>
2023-07-26 15:12:28 +0300

    Exercise 8 done

---
+++ csharp-basics/exercises/TypesAndVariables/Exercise 8/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercise_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter minutes: ");
+            int minutes = int.Parse(Console.ReadLine());
+
+            const int minutesInDay = 24 * 60;
+            const int minutesInYear = 365 * minutesInDay;
+
+            int years = minutes / minutesInYear;
+            int remainingMinutes = minutes % minutesInYear;
+            int days = remainingMinutes / minutesInDay;
+
+            Console.WriteLine($"{minutes} minutes is {years} years and {days} days.");
+        }
+    }
+}
