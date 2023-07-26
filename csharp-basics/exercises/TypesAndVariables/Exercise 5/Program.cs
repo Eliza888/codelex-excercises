d66cbda5e3f2fc24b129ffebc98ed6d4d4ca4b02
Elīza Ivenševa <e.ivenseva@gmail.com>
2023-07-26 12:54:40 +0300

    Exercise 5 done

--- csharp-basics/exercises/TypesAndVariables/Exercise 5/Program.cs
+++ csharp-basics/exercises/TypesAndVariables/Exercise 5/Program.cs
@@ -5,37 +5,35 @@
         static void Main(string[] args)
         {
 
-            string[] classNames = {
-            "English III",
-            "Precalculus",
-            "Music Theory",
-            "Biotechnology",
-            "Principles of Technology I",
-            "Latin II",
-            "AP US History",
-            "Business Computer Information Systems"
-        };
+            string class1 = "English III";
+            string class2 = "Precalculus";
+            string class3 = "Music Theory";
+            string class4 = "Biotechnology";
+            string class5 = "Principles of Technology I";
+            string class6 = "Latin II";
+            string class7 = "AP US History";
+            string class8 = "Business Computer Information Systems";
 
-            string[] teacherNames = {
-            "Ms. Lapan",
-            "Mrs. Gideon",
-            "Mr. Davis",
-            "Ms. Palmer",
-            "Ms. Garcia",
-            "Mrs. Barnett",
-            "Ms. Johannessen",
-            "Mr. James"
-        };
+            string teacher1 = "Ms. Lapan";
+            string teacher2 = "Mrs. Gideon";
+            string teacher3 = "Mr. Davis";
+            string teacher4 = "Ms. Palmer";
+            string teacher5 = "Ms. Garcia";
+            string teacher6 = "Mrs. Barnett";
+            string teacher7 = "Ms. Johannessen";
+            string teacher8 = "Mr. James";
 
             Console.WriteLine("+------------------------------------------------------------+");
-            Console.WriteLine("| 1 | " + classNames[0].PadRight(26) + " | " + teacherNames[0].PadRight(15) + " |");
-            Console.WriteLine("| 2 | " + classNames[1].PadRight(26) + " | " + teacherNames[1].PadRight(15) + " |");
-            Console.WriteLine("| 3 | " + classNames[2].PadRight(26) + " | " + teacherNames[2].PadRight(15) + " |");
-            Console.WriteLine("| 4 | " + classNames[3].PadRight(26) + " | " + teacherNames[3].PadRight(15) + " |");
-            Console.WriteLine("| 5 | " + classNames[4].PadRight(26) + " | " + teacherNames[4].PadRight(15) + " |");
-            Console.WriteLine("| 6 | " + classNames[5].PadRight(26) + " | " + teacherNames[5].PadRight(15) + " |");
-            Console.WriteLine("| 7 | " + classNames[6].PadRight(26) + " | " + teacherNames[6].PadRight(15) + " |");
-            Console.WriteLine("| 8 | " + classNames[7].PadRight(26) + " | " + teacherNames[7].PadRight(15) + " |");
+            Console.WriteLine("| # | Class Name                  | Teacher Name    |");
+            Console.WriteLine("+------------------------------------------------------------+");
+            Console.WriteLine("| 1 | " + class1.PadRight(26) + " | " + teacher1.PadRight(15) + " |");
+            Console.WriteLine("| 2 | " + class2.PadRight(26) + " | " + teacher2.PadRight(15) + " |");
+            Console.WriteLine("| 3 | " + class3.PadRight(26) + " | " + teacher3.PadRight(15) + " |");
+            Console.WriteLine("| 4 | " + class4.PadRight(26) + " | " + teacher4.PadRight(15) + " |");
+            Console.WriteLine("| 5 | " + class5.PadRight(26) + " | " + teacher5.PadRight(15) + " |");
+            Console.WriteLine("| 6 | " + class6.PadRight(26) + " | " + teacher6.PadRight(15) + " |");
+            Console.WriteLine("| 7 | " + class7.PadRight(26) + " | " + teacher7.PadRight(15) + " |");
+            Console.WriteLine("| 8 | " + class8.PadRight(26) + " | " + teacher8.PadRight(15) + " |");
             Console.WriteLine("+------------------------------------------------------------+");
         }
     }
