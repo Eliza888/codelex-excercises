ef6cdaa729337f485e5a47988b57d6f9d75bd911
Elīza Ivenševa <e.ivenseva@gmail.com>
2023-07-26 12:35:17 +0300

    Exercise 4 done

---
+++ csharp-basics/exercises/TypesAndVariables/Exercise 4/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Exercise_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName = "Juan Valdez";
+            int BirthYear = 1995;
+
+            Console.WriteLine("My name is " + MyName + " and I was born in " + BirthYear + ".");
+        }
+    }
+}
