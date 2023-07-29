1918e524a61b4ee8a0b7ab875e814aa44ceb02cb
Elīza Ivenševa <e.ivenseva@gmail.com>
2023-07-29 10:44:28 +0300

    Exercise 1 done

---
+++ csharp-basics/exercises/Arithmetic/Exercise1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using System;
+
+namespace Exercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First integer:");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Second integer:");
+            int num2 = int.Parse(Console.ReadLine());
+
+            bool result = (num1 == 15 || num2 == 15 || num1 + num2 == 15 || Math.Abs(num1 - num2) == 15);
+
+            Console.WriteLine(result);
+        }
+    }
+}
+
+
+    
