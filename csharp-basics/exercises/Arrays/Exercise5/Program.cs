0349ed9bfba56b548ae449960edd3a4eb13b0c06
Elīza Ivenševa <e.ivenseva@gmail.com>
2023-08-01 19:46:21 +0300

    Exercise 5

--- csharp-basics/exercises/Arrays/Exercise5/Program.cs
+++ csharp-basics/exercises/Arrays/Exercise5/Program.cs
@@ -11,20 +11,27 @@ namespace Exercise5
 
             var indexOfElement36 = -1;
             var indexOfElement29 = -1;
+           
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                if (myArray[i] == 36 && indexOfElement36 == -1)
+                {
+                    indexOfElement36 = i;
+                }
+                else if (myArray[i] == 29 && indexOfElement29 == -1)
+                {
+                    indexOfElement29 = i;
+                }
 
-            /*
-            fixme
-            for (?) {
-                
+                // If both elements are found, exit the loop early
+                if (indexOfElement36 != -1 && indexOfElement29 != -1)
+                {
+                    break;
+                }
             }
-            */
+
             Console.WriteLine("Index position of 36 is: " + indexOfElement36);
             Console.WriteLine("Index position of 29 is: " + indexOfElement29);
-
-            //Expected output:
-            //Index position of 36 is: 4
-            //Index position of 29 is: 8
-
         }
     }
 }
