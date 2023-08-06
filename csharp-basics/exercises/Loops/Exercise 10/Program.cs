2eac921d7366a4e5d4a8c67e8cff773d8b9743f3
Elīza Ivenševa <e.ivenseva@gmail.com>
2023-08-06 21:18:33 +0300

    Exercise 10 done

---
+++ csharp-basics/exercises/Loops/Exercise 10/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Exercise_10
+{
+    class NumberSquare
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Min? ");
+            int min = int.Parse(Console.ReadLine());
+
+            Console.Write("Max? ");
+            int max = int.Parse(Console.ReadLine());
+
+            int range = max - min + 1;
+
+            for (int i = 0; i < range; i++)
+            {
+                for (int j = 0; j < range; j++)
+                {
+                    int num = (min + i + j - 1) % range + min;
+                    Console.Write(num);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
