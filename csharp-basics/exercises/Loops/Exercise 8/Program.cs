1f8b1a0546d249a8e4541d28db9c64afc9b505a3
Elīza Ivenševa <e.ivenseva@gmail.com>
2023-08-06 18:14:14 +0300

    Exercise 8 done

---
+++ csharp-basics/exercises/Loops/Exercise 8/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Exercise_8
+{
+    class AsciiFigure
+    {
+        static int SIZE = 55;
+
+        public void Draw()
+        {
+            for (int i = 0; i < SIZE; i++)
+            {
+                for (int j = 0; j < SIZE * 2 - 2 - 2 * i; j++)
+                {
+                    Console.Write('/');
+                }
+                for (int j = 0; j < 4 * i; j++)
+                {
+                    Console.Write('*');
+                }
+                for (int j = 0; j < SIZE * 2 - 2 - 2 * i; j++)
+                {
+                    Console.Write('\\');
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            AsciiFigure figure = new AsciiFigure();
+            figure.Draw();
+        }
+    }
+}
