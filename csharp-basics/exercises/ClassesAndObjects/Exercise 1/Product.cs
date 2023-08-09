205298613e139929d96ff3b82199fe9edcd00b61
Elīza Ivenševa <e.ivenseva@gmail.com>
2023-08-09 16:34:37 +0300

    Exercise 1 done

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 1/Product.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_1
+{
+    class Product
+    {
+        public string Name { get; private set; }
+        public double Price { get; private set; }
+        public int Amount { get; private set; }
+
+        public Product(string name, double priceAtStart, int amountAtStart)
+        {
+            Name = name;
+            Price = priceAtStart;
+            Amount = amountAtStart;
+        }
+
+        public void PrintProduct()
+        {
+            Console.WriteLine($"{Name}, price {Price.ToString("F2")} EUR, amount {Amount}");
+        }
+
+        public void ChangeQuantity(int newAmount)
+        {
+            if (newAmount >= 0)
+            {
+                Amount = newAmount;
+            }
+            else
+            {
+                Console.WriteLine("Invalid quantity.");
+            }
+        }
+
+        public void ChangePrice(double newPrice)
+        {
+            if (newPrice >= 0)
+            {
+                Price = newPrice;
+            }
+            else
+            {
+                Console.WriteLine("Invalid price.");
+            }
+        }
+    }
+} 
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product mouse = new Product("Logitech mouse", 70.00, 14);
+            Product iphone = new Product("iPhone 5s", 999.99, 3);
+            Product projector = new Product("Epson EB-U05", 440.46, 1);
+
+            mouse.PrintProduct();
+            iphone.PrintProduct();
+            projector.PrintProduct();
+
+            Console.WriteLine();
+
+            mouse.ChangeQuantity(20);
+            iphone.ChangePrice(899.99);
+            projector.ChangeQuantity(2);
+
+            mouse.PrintProduct();
+            iphone.PrintProduct();
+            projector.PrintProduct();
+        }
+    }
+}
