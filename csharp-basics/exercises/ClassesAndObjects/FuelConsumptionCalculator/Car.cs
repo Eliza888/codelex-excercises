66ee4c6d8c7061cb137d58b4d9efde0e8f2ddefe
Elīza Ivenševa <e.ivenseva@gmail.com>
2023-08-09 17:07:11 +0300

    Exercise 2 done

---
+++ csharp-basics/exercises/ClassesAndObjects/FuelConsumptionCalculator/Car.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace FuelConsumptionCalculator
+{
+    public class Car
+    {
+        private double startKilometers; // Starting odometer reading
+        private double endKilometers; // Ending odometer reading
+        private double liters; // Liters of gas used between the readings
+
+        public Car(double startOdo, double endOdo = 0, double gasLiters = 0)
+        {
+            startKilometers = startOdo;
+            endKilometers = endOdo;
+            liters = gasLiters;
+        }
+
+        public void FillUp(int mileage, double gasLiters)
+        {
+            endKilometers = mileage;
+            liters += gasLiters;
+        }
+
+        public double CalculateConsumption()
+        {
+            if (endKilometers == startKilometers)
+            {
+                return 0.0; // Avoid division by zero
+            }
+            return (liters / (endKilometers - startKilometers)) * 100;
+        }
+
+        public bool IsGasHog()
+        {
+            return CalculateConsumption() > 15.0;
+        }
+
+        public bool IsEconomyCar()
+        {
+            return CalculateConsumption() < 5.0;
+        }
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/FuelConsumptionCalculator/CarTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Numerics;
+
+namespace FuelConsumptionCalculator
+{
+    class CarTest
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car(1000);
+            car1.FillUp(1200, 50);
+            car1.FillUp(1500, 40);
+
+            Car car2 = new Car(2000);
+            car2.FillUp(2200, 60);
+            car2.FillUp(2500, 30);
+
+            Console.WriteLine("Car 1 Consumption: " + car1.CalculateConsumption() + " l/100 km");
+            Console.WriteLine("Car 1 Gas Hog: " + car1.IsGasHog());
+            Console.WriteLine("Car 1 Economy Car: " + car1.IsEconomyCar());
+
+            Console.WriteLine("Car 2 Consumption: " + car2.CalculateConsumption() + " l/100 km");
+            Console.WriteLine("Car 2 Gas Hog: " + car2.IsGasHog());
+            Console.WriteLine("Car 2 Economy Car: " + car2.IsEconomyCar());
+        }
+    }
+}
