11918caa48eaba5fc79bd73cd55cf63f301e00f4
Elīza Ivenševa <e.ivenseva@gmail.com>
2023-08-10 18:54:08 +0300

    Exercise 7 done

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 7/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Exercise_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How much money is in the account?: ");
+            double startingBalance = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the annual interest rate: ");
+            double annualInterestRate = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("How long has the account been opened? ");
+            int months = Convert.ToInt32(Console.ReadLine());
+
+            SavingsAccount savingsAccount = new SavingsAccount(startingBalance);
+            savingsAccount.SetAnnualInterestRate(annualInterestRate);
+
+            double totalDeposits = 0;
+            double totalWithdrawals = 0;
+
+            for (int i = 1; i <= months; i++)
+            {
+                Console.Write($"Enter amount deposited for month {i}: ");
+                double depositAmount = Convert.ToDouble(Console.ReadLine());
+                savingsAccount.MakeDeposit(depositAmount);
+                totalDeposits += depositAmount;
+
+                Console.Write($"Enter amount withdrawn for {i}: ");
+                double withdrawalAmount = Convert.ToDouble(Console.ReadLine());
+                savingsAccount.MakeWithdrawal(withdrawalAmount);
+                totalWithdrawals += withdrawalAmount;
+
+                savingsAccount.CalculateMonthlyInterest();
+            }
+
+            double totalInterestEarned = savingsAccount.GetBalance() - (startingBalance + totalDeposits - totalWithdrawals);
+
+            Console.WriteLine($"Total deposited: ${totalDeposits:F2}");
+            Console.WriteLine($"Total withdrawn: ${totalWithdrawals:F2}");
+            Console.WriteLine($"Interest earned: ${totalInterestEarned:F2}");
+            Console.WriteLine($"Ending balance: ${savingsAccount.GetBalance():F2}");
+        }
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 7/SavingsAccount.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_7
+{
+    internal class SavingsAccount
+    {
+        private double balance;
+        private double annualInterestRate;
+
+        public SavingsAccount(double startingBalance)
+        {
+            balance = startingBalance;
+        }
+
+        public void MakeWithdrawal(double amount)
+        {
+            balance -= amount;
+        }
+
+        public void MakeDeposit(double amount)
+        {
+            balance += amount;
+        }
+
+        public void CalculateMonthlyInterest()
+        {
+            double monthlyInterestRate = annualInterestRate / 12.0;
+            double interestEarned = balance * monthlyInterestRate;
+            balance += interestEarned;
+        }
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+
+        public void SetAnnualInterestRate(double rate)
+        {
+            annualInterestRate = rate;
+        }
+    }
+}
