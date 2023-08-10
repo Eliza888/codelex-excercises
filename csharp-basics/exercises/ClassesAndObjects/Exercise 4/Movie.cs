a9021685f5b8e76665f49984f1a175797b441429
Elīza Ivenševa <e.ivenseva@gmail.com>
2023-08-10 14:17:54 +0300

    Exercise 4 done

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 4/Movie.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_4
+{
+    internal class Movie
+    {
+        private string title;
+        private string studio;
+        private string rating;
+
+        public Movie(string title, string studio, string rating)
+        {
+            this.title = title;
+            this.studio = studio;
+            this.rating = rating;
+        }
+
+        public Movie(string title, string studio)
+        {
+            this.title = title;
+            this.studio = studio;
+            this.rating = "PG";
+        }
+
+        public string Title
+        {
+            get { return title; }
+        }
+
+        public string Studio
+        {
+            get { return studio; }
+        }
+
+        public string Rating
+        {
+            get { return rating; }
+        }
+
+        public static Movie[] GetPG(Movie[] movies)
+        {
+            return movies.Where(movie => movie.Rating == "PG").ToArray();
+        }
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 4/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Exercise_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie movie1 = new Movie("Casino Royale", "Eon Productions", "PG-13");
+            Movie movie2 = new Movie("Glass", "Buena Vista International", "PG-13");
+            Movie movie3 = new Movie("Spider-Man: Into the Spider-Verse", "Columbia Pictures", "PG");
+            Movie movie4 = new Movie("The Matrix", "Warner Bros.", "R");
+
+            Movie[] movies = { movie1, movie2, movie3, movie4 };
+
+            Movie[] pgMovies = Movie.GetPG(movies);
+
+            Console.WriteLine("Movies with a rating of 'PG':");
+            foreach (var movie in pgMovies)
+            {
+                Console.WriteLine($"Title: {movie.Title}, Studio: {movie.Studio}, Rating: {movie.Rating}");
+            }
+        }
+    }
+}
