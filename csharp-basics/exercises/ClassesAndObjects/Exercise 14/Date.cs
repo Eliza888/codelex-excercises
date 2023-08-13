b117142384c68e269769cd22135fe22a831e45d2
Elīza Ivenševa <e.ivenseva@gmail.com>
2023-08-13 16:13:32 +0300

    Exercise 14 done

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 14/Date.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_14
+{
+    internal class Date
+    {
+        public static string WeekdayInDutch(int year, int month, int day)
+        {
+            DateTime date = new DateTime(year, month, day);
+            CultureInfo dutchCulture = new CultureInfo("nl-NL");
+            return date.ToString("dddd", dutchCulture);
+        }
+    }
+}
