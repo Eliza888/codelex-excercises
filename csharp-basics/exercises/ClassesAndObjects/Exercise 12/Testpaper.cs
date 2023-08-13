7d4f34ed85f19d566d03ba3ddba0630c87b4f32e
Elīza Ivenševa <e.ivenseva@gmail.com>
2023-08-13 16:04:50 +0300

    Exercise 12 done

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 12/Testpaper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Exercise_12
+{
+    class Testpaper : ITestpaper
+    {
+        public string Subject { get; }
+        public string[] MarkScheme { get; }
+        public string PassMark { get; }
+
+        public Testpaper(string subject, string[] markScheme, string passMark)
+        {
+            Subject = subject;
+            MarkScheme = markScheme;
+            PassMark = passMark;
+        }
+    }
+}
