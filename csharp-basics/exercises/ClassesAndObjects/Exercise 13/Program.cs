0e63790e14426982be4f8fa724064ac1d475e108
Elīza Ivenševa <e.ivenseva@gmail.com>
2023-08-13 16:08:57 +0300

    Exercise 13 done

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 13/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercise_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                Smoothie s1 = new Smoothie(new string[] { "Banana" });
+                Console.WriteLine(string.Join(", ", s1.Ingredients));
+                Console.WriteLine($"Cost: £{s1.GetCost():F2}");
+                Console.WriteLine($"Price: £{s1.GetPrice():F2}");
+                Console.WriteLine($"Name: {s1.GetName()}");
+
+                Smoothie s2 = new Smoothie(new string[] { "Raspberries", "Strawberries", "Blueberries" });
+                Console.WriteLine(string.Join(", ", s2.Ingredients));
+                Console.WriteLine($"Cost: £{s2.GetCost():F2}");
+                Console.WriteLine($"Price: £{s2.GetPrice():F2}");
+                Console.WriteLine($"Name: {s2.GetName()}");
+            }
+        }
+    }
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 13/Smoothie.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_13
+{
+    internal class Smoothie
+    {
+        private Dictionary<string, decimal> ingredientPrices = new Dictionary<string, decimal>
+    {
+        { "Strawberries", 1.50m },
+        { "Banana", 0.50m },
+        { "Mango", 2.50m },
+        { "Blueberries", 1.00m },
+        { "Raspberries", 1.00m },
+        { "Apple", 1.75m },
+        { "Pineapple", 3.50m }
+    };
+
+        public List<string> Ingredients { get; }
+
+        public Smoothie(string[] ingredients)
+        {
+            Ingredients = new List<string>(ingredients);
+        }
+
+        public decimal GetCost()
+        {
+            return Ingredients.Sum(ingredient => ingredientPrices.GetValueOrDefault(ingredient, 0m));
+        }
+
+        public decimal GetPrice()
+        {
+            return Math.Round(GetCost() * 2.5m, 2, MidpointRounding.AwayFromZero);
+        }
+
+        public string GetName()
+        {
+            string[] sortedIngredients = Ingredients.Select(ingredient => ingredient.EndsWith("ies") ? ingredient.TrimEnd('s') + "y" : ingredient)
+                                                    .OrderBy(ingredient => ingredient)
+                                                    .ToArray();
+
+            string name = string.Join(" ", sortedIngredients);
+            return Ingredients.Count > 1 ? $"{name} Fusion" : $"{name} Smoothie";
+        }
+    }
+}
