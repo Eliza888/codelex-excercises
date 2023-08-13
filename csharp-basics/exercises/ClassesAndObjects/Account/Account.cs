cd5ee5d2ebe6c61f88d3ddb25af50572a02419fb
Elīza Ivenševa <e.ivenseva@gmail.com>
2023-08-13 15:50:44 +0300

    Exercise 11 done

--- csharp-basics/exercises/ClassesAndObjects/Account/Account.cs
+++ csharp-basics/exercises/ClassesAndObjects/Account/Account.cs
@@ -2,6 +2,41 @@
 {
     class Account
     {
-        
+        private string accountName;
+        private double balance;
+
+        public Account(string accountName, double initialBalance)
+        {
+            this.accountName = accountName;
+            this.balance = initialBalance;
+        }
+
+        public void Deposit(double amount)
+        {
+            if (amount > 0)
+            {
+                balance += amount;
+            }
+        }
+
+        public bool Withdrawal(double amount)
+        {
+            if (amount > 0 && balance >= amount)
+            {
+                balance -= amount;
+                return true;
+            }
+            return false;
+        }
+
+        public double Balance()
+        {
+            return balance;
+        }
+
+        public override string ToString()
+        {
+            return $"{accountName} balance: {balance:F2}";
+        }
     }
 }
--- csharp-basics/exercises/ClassesAndObjects/Account/Program.cs
+++ csharp-basics/exercises/ClassesAndObjects/Account/Program.cs
@@ -1,9 +1,27 @@
-﻿namespace Account
+﻿using System;
+
+namespace Account
 {
     class Program
     {
-        private static void Main(string[] args)
+        static void Main(string[] args)
         {
+            Account bartosAccount = new Account("Barto's account", 100.00);
+            Account bartosSwissAccount = new Account("Barto's account in Switzerland", 1000000.00);
+
+            Console.WriteLine("Initial state");
+            Console.WriteLine(bartosAccount);
+            Console.WriteLine(bartosSwissAccount);
+
+            bartosAccount.Withdrawal(20);
+            Console.WriteLine("Barto's account balance is now: " + bartosAccount.Balance());
+
+            bartosSwissAccount.Deposit(200);
+            Console.WriteLine("Barto's Swiss account balance is now: " + bartosSwissAccount.Balance());
+
+            Console.WriteLine("Final state");
+            Console.WriteLine(bartosAccount);
+            Console.WriteLine(bartosSwissAccount);
         }
     }
 }
