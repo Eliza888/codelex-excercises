8cd1411106647aec85fbbe72cc0d7d1b83ab93c1
Elīza Ivenševa <e.ivenseva@gmail.com>
2023-08-14 15:31:11 +0300

    List 3 done

--- csharp-basics/exercises/Collections/ListExercise3/Program.cs
+++ csharp-basics/exercises/Collections/ListExercise3/Program.cs
@@ -7,7 +7,6 @@ namespace ListExercise3
     {
         static void Main(string[] args)
         {
-            //TODO: Write a C# program to insert an element into the array list at the first position.
 
             var colors = new List<string>();
             colors.Add("Red");
@@ -16,11 +15,10 @@ namespace ListExercise3
             colors.Add("White");
             colors.Add("Black");
 
-            Console.WriteLine(string.Join(",", colors));
+            colors.Insert(0, "Purple");
+            colors.Insert(2, "Yellow");
 
-            //TODO: Add new color at the first and third position of the list
-
-            //Print the list
+            Console.WriteLine("Updated list with colors inserted:");
             Console.WriteLine(string.Join(",", colors));
         }
     }
