a6a89f58347f55668b9ee689ff3c0e9a3da4c2d8
Elīza Ivenševa <e.ivenseva@gmail.com>
2023-08-14 19:23:02 +0300

    Linq 2 done

--- csharp-basics/exercises/Collections/DecryptNumber/Program.cs
+++ csharp-basics/exercises/Collections/DecryptNumber/Program.cs
@@ -17,6 +17,37 @@ namespace DecryptNumber
                 "!)(^&(#@",
                 "!)(#&%(*@#%"
             };
+
+
+            foreach (var crypted in cryptedNumbers)
+            {
+                long sum = 0;
+                foreach (char c in crypted)
+                {
+                    int value = GetCharacterValue(c);
+                    sum = sum * 10 + value;
+                }
+
+                Console.WriteLine($"Crypted: {crypted}, Decrypted number: {sum}");
+            }
+        }
+
+        static int GetCharacterValue(char c)
+        {
+            switch (c)
+            {
+                case '!': return 1;
+                case '@': return 2;
+                case '#': return 3;
+                case '$': return 4;
+                case '%': return 5;
+                case '^': return 6;
+                case '&': return 7;
+                case '*': return 8;
+                case '(': return 9;
+                case ')': return 0;
+                default: return 0;
+            }
         }
     }
 }
