6645ddda57f1d6e3ee41e65c96d7f01c5b772256
Elīza Ivenševa <e.ivenseva@gmail.com>
2023-08-14 19:12:20 +0300

    linq1 done

--- csharp-basics/exercises/Collections/DaysNames/Program.cs
+++ csharp-basics/exercises/Collections/DaysNames/Program.cs
@@ -1,3 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-//ToDo: Write a query that returns names of days
-//https://docs.microsoft.com/en-us/dotnet/api/system.dayofweek?view=net-6.0
+﻿using System;
+using System.Linq;
+
+namespace DayNamesQuery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] daysOfWeek = {
+                "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+            };
+
+            var dayNamesQuery = from day in daysOfWeek
+                                select day;
+
+            Console.WriteLine("Names of days:");
+            foreach (var dayName in dayNamesQuery)
+            {
+                Console.WriteLine(dayName);
+            }
+        }
+    }
+}
