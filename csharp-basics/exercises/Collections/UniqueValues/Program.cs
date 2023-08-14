9d129d4c12b6e450438678ae0a135389d1302720
Elīza Ivenševa <e.ivenseva@gmail.com>
2023-08-14 19:37:40 +0300

    Linq 5 done

--- csharp-basics/exercises/Collections/UniqueValues/Program.cs
+++ csharp-basics/exercises/Collections/UniqueValues/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace UniqueValues
 {
@@ -11,6 +13,15 @@ namespace UniqueValues
 
             var values = new List<string> { "Hi", "Meow", "Hello", "Meow", "Hi!", "Meow", "Hi", "Bye" };
 
+            var nonRepeatingValues = values.GroupBy(value => value)
+                                                       .Where(group => group.Count() == 1)
+                                                       .Select(group => group.Key)
+                                                       .ToList();
+
+            foreach (var value in nonRepeatingValues)
+            {
+                Console.WriteLine(value);
+            }
         }
     }
 }
