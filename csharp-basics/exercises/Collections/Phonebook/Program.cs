7cdca5cf04198870ee9f54a35acdec9845413828
Elīza Ivenševa <e.ivenseva@gmail.com>
2023-08-17 17:15:54 +0300

    Exercise 9 done

--- csharp-basics/exercises/Collections/Phonebook/Program.cs
+++ csharp-basics/exercises/Collections/Phonebook/Program.cs
@@ -1,9 +1,21 @@
-﻿namespace Phonebook
+﻿using System;
+
+namespace PhoneBook
 {
     class Program
     {
         static void Main(string[] args)
         {
+            PhoneDirectory phoneDirectory = new PhoneDirectory();
+
+            phoneDirectory.PutNumber("Alice", "54625676");
+            phoneDirectory.PutNumber("John", "65786573");
+
+            string aliceNumber = phoneDirectory.GetNumber("Alice");
+            string bobNumber = phoneDirectory.GetNumber("John");
+
+            Console.WriteLine("Alices number: " + aliceNumber);
+            Console.WriteLine("Johns number: " + bobNumber);
         }
     }
 }
